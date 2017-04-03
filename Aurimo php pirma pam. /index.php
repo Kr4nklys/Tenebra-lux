7f4dc616a0f2a188dc36b26fa98b366f5ebb7ef1
Kr4nklys <justas.vaitkevicius92@gmail.com>
2017-04-03 21:22:58 +0300

    php pirma pamoka pirma dalis

---
+++ Aurimo php pirma pam. /index.php
@@ -0,0 +1,28 @@
+<?php 
+
+// $a = 72;
+// $b = 65;
+// echo $a;
+// echo "*";
+// echo $b;
+// echo "=";
+// echo $a * $b;
+
+$dictionary = [
+	'hello' => 'labas',
+	'Trump' => 'Oranzinis orangutanas, kuris meluoja',
+	'truth' => 'tiesa',
+	'symbol' => 'simbolis',
+	'apple' => 'obuolys',
+	'night' => 'naktis',
+	'wednesday' => 'treciadienis',
+	'shadow' => 'seselis',
+	'blood' => 'kraujas',
+	'moon' => 'menulis'
+];
+
+$werd = $_GET['werd'];
+	
+// includes of html
+	include 'index.html';
+ ?>
