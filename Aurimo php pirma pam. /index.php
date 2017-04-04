d328a48e0730eaf9a9c6f1b9a234bcb3dd53da41
Kr4nklys <justas.vaitkevicius92@gmail.com>
2017-04-04 21:33:09 +0300

    finished translatour

--- Aurimo php pirma pam. /index.php
+++ Aurimo php pirma pam. /index.php
@@ -7,22 +7,36 @@
 // echo $b;
 // echo "=";
 // echo $a * $b;
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
 $dictionary = [
-	'hello' => 'labas',
-	'Trump' => 'Oranzinis orangutanas, kuris meluoja',
-	'truth' => 'tiesa',
-	'symbol' => 'simbolis',
-	'apple' => 'obuolys',
-	'night' => 'naktis',
-	'wednesday' => 'treciadienis',
-	'shadow' => 'seselis',
-	'blood' => 'kraujas',
-	'moon' => 'menulis'
+'hello' => 'labas',
+'evil' => 'blogis',
+'truth' => 'tiesa',
+'symbol' => 'simbolis',
+'apple' => 'obuolys',
+'night' => 'naktis',
+'wednesday' => 'treciadienis',
+'shadow' => 'seselis',
+'blood' => 'kraujas',
+'moon' => 'menulis'
 ];
 
-$werd = $_GET['werd'];
-	
+$originalWerd = '';
+if (array_key_exists('werd', $_GET)) {
+	$originalWerd = ($_GET['werd']);	
+	$werd = strtolower($_GET['werd']);
+
+if ($_GET['direction'] == 'toEng') {
+	$phrase = array_search($werd, $dictionary);
+}
+else{
+	if(array_key_exists($werd, $dictionary)) {
+	$phrase = $dictionary[$werd];
+	}
+}
+}
 // includes of html
-	include 'index.html';
- ?>
+include 'index.html';
+?>
